b375ac65cff5c9252726df9ea8ba41c72460c4fc
David Kezi <davidkezi6@gmail.com>
2019-03-14 02:53:43 +0100

    feat: morse challenge done

--- src/morse.js
+++ src/morse.js
@@ -58,13 +58,46 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-/**
- * This is the entry point to the program.
- *
- * @param {string} morseCode The string to decode.
- */
 function decodeMorse(morseCode) {
-  // Your code should go here.
+
+  if (morseCode === "") { return "" }
+  const trimmedMorse = morseCode.trim();
+  let splittedMorse = trimmedMorse.split(' ');
+  return splittedMorse.length > 1 ? decodeSplitted() : decodeSingle();
+
+  function decodeSplitted() {
+    let arrayMorse = splittedMorse.map((value) => {
+      if (value == ' ') return ' ';
+      return MORSE_CODE[value];
+    });
+
+    let thereIsASpace = false;
+
+    arrayMorse.forEach((item, index, array) => {
+      if (index > 0 && !array[index] && !array[index + 1]) {
+        thereIsASpace = true;
+      }
+      if (index === arrayMorse.length - 1 && !thereIsASpace) {
+        arrayMorse = arrayMorse.join('');
+      }
+    })
+
+    if (thereIsASpace) {
+      arrayMorse.map((item, index, array) => {
+        if (!item) array[index] = 'a';
+      })
+      arrayMorse = arrayMorse.join('')
+      arrayMorse = arrayMorse.replace(/[a][a]/g, ' ');
+      return arrayMorse;
+    }
+    else {
+      return arrayMorse;
+    }
+  }
+  function decodeSingle() {
+    return MORSE_CODE[splittedMorse];
+  }
 }
+
 
 module.exports = decodeMorse;
